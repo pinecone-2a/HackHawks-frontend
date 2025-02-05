1e5238ff25791744b41184dc0ecdee63178d6202
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-05 10:25:46 +0800

    working on step 2

--- src/app/profile-setup/page.tsx
+++ src/app/profile-setup/page.tsx
@@ -1,9 +1,9 @@
 import { Input } from "@/components/ui/input";
-import { ProfileHeader } from "./_components/create_profile_header";
+import { ProfileHeader } from "./_components/profile_header";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import ProfileSetup1 from "./_components/create_profile_setup_form_step1";
-import ProfileSetup2 from "./_components/create_profile_setup_form_step2";
+import ProfileSetup1 from "./_components/profile_setup_form_step1";
+import ProfileSetup2 from "./_components/profile_setup_form_step2";
 import Link from "next/link";
 type Props = {
   searchParams: Promise<{
@@ -17,7 +17,7 @@ export default async function Profile(props: Props) {
       <div className="py-6">
         <ProfileHeader />
       </div>
-      {!step && <Link href={`/profile?step=1`}>Click here dummy</Link>}
+      {!step && <Link href={`/profile-setup?step=1`}>Click here dummy</Link>}
       {Number(step) == 1 && (
         <div className="flex justify-center items-center min-h-screen">
           <ProfileSetup1 />
