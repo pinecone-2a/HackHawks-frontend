b59b1ea98f3671fc000d57edf2aa1a9d3e62993f
24LP6048 24LP6048 <24LP6048@G302-CC0121.local>
2025-02-06 19:44:18 +0800

    Creator-page

---
+++ src/app/_components/creator-page/page.tsx
@@ -0,0 +1,12 @@
+
+import { Button } from "@/components/ui/button"
+import { Navigation } from "../Navigation"
+
+export function Home() {
+    return (
+        <div>
+            <Navigation/>
+
+        </div>
+    )
+}
