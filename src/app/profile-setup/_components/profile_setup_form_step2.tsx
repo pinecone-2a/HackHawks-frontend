e0deefd1704b278832222e4cfc089ea2041dbafe
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-07 12:43:52 +0800

    validation + zod

--- src/app/profile-setup/_components/profile_setup_form_step2.tsx
+++ src/app/profile-setup/_components/profile_setup_form_step2.tsx
@@ -4,6 +4,8 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { ChangeEvent, useEffect, useState } from "react";
 import { UserInfoForm } from "../../utils/types";
+import { z } from "zod";
+import { form } from "@/app/account/_components/step1";
 type country = {
   name: {
     common: string;
@@ -11,12 +13,51 @@ type country = {
   cios: string;
   cca2: string;
 };
+
+const paymentSchema = z.object({
+  country: z.string(),
+  firstName: z.string().min(6),
+  lastName: z.string().min(6),
+  cardNumber: z.string().min(16),
+  expiryDate: z.date(),
+});
 export default function ProfileSetup2() {
+  const [cardExpiryDate, setCardExpiryDate] = useState("");
   const [countries, setCountries] = useState<country[]>([]);
+  const [isValid, setValid] = useState<boolean>(false);
+  const [form2, setForm2] = useState({
+    country: "",
+    firstName: "",
+    lastName: "",
+    cardNumber: "",
+    expiryDate: "",
+  });
+  const [form1, setForm1] = useState<UserInfoForm>({
+    name: "",
+    about: "",
+    socialMediaURL: "",
+    avatarImage: "",
+  });
   const months = Array.from({ length: 12 }, (_, i) => i + 1);
   const currentYear = new Date().getFullYear();
   const years = Array.from({ length: 10 }, (_, i) => currentYear + i);
 
+  useEffect(() => {
+    localStorage.setItem(
+      "step2",
+      JSON.stringify({
+        country: form2.country,
+        firstName: form2.firstName,
+        lastName: form2.lastName,
+      })
+    );
+    const result = paymentSchema.safeParse(form2);
+    if (result.success) {
+      setValid(true);
+    } else {
+      setValid(false);
+    }
+  }, [form2]);
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(`https://restcountries.com/v3.1/all`);
@@ -26,7 +67,24 @@ export default function ProfileSetup2() {
     };
     fetchData();
   }, []);
-
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement | HTMLInputElement>
+  ) => {
+    const { name, value } = e.target;
+    setForm2((p) => {
+      return {
+        ...p,
+        [name]: value,
+      };
+    });
+    console.log(form2);
+  };
+  useEffect(() => {
+    const formString = localStorage.getItem("step1");
+    const formL = formString ? JSON.parse(formString) : {};
+    setForm1(formL);
+  }, []);
+  console.log(form1);
   return (
     <div className="w-[510px] h-[631px] flex flex-col gap-10">
       <div>
@@ -38,7 +96,12 @@ export default function ProfileSetup2() {
       <div className="flex flex-col gap-10">
         <div className="font-semibold ">
           <label htmlFor="countries">Select country</label>
-          <select id="countries" className="w-full border p-2 rounded-md">
+          <select
+            onChange={handleChange}
+            name="country"
+            id="countries"
+            className="w-full border p-2 rounded-md"
+          >
             {countries &&
               countries.map((country: country) => (
                 <option key={country.cca2} value={country.name.common}>
@@ -52,14 +115,20 @@ export default function ProfileSetup2() {
           <div>
             <label htmlFor="firstname">First name</label>
             <Input
+              onChange={handleChange}
               id="firstname"
-              name="firstname"
+              name="firstName"
               placeholder="your first name"
             />
           </div>
           <div>
             <label htmlFor="lastname">Last name</label>
-            <Input id="lastname" name="lastname" placeholder="your last name" />
+            <Input
+              onChange={handleChange}
+              id="lastname"
+              name="lastName"
+              placeholder="your last name"
+            />
           </div>
         </div>
 
@@ -69,8 +138,9 @@ export default function ProfileSetup2() {
         >
           <label htmlFor="card-number">Enter card number</label>
           <Input
+            onChange={handleChange}
             id="card-number"
-            name="card"
+            name="cardNumber"
             placeholder="XXXX-XXXX-XXXX-XXXX"
           />
         </div>
@@ -111,8 +181,9 @@ export default function ProfileSetup2() {
           </div>
         </div>
       </div>
-      <div className="flex justify-end">
+      <div className="flex justify-end text-background">
         <Button
+          disabled={!isValid}
           onClick={() => {
             console.log("it works");
           }}
