695b0222e18544915d2ba113d8456c4ebb37dc1c
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2025-02-07 19:19:00 +0800

    do

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-'use client';
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
 import { useState } from "react";
-import EditProfile from './_components/EditProfile';
-import { CreatorPage } from './_components/creator-page/CreatorPage';
+import EditProfile from "./_components/EditProfile";
+import { CreatorPage } from "./_components/creator-page/CreatorPage";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,10 +10,11 @@ export default function Home() {
   return (
     <div>
       hi2 hello
-      <button onClick={() => setIsOpen(true)} className="mt-4 px-4 py-2 bg-[#F4F4F5] text-[#18181B] rounded-md">Edit Page</button>
+      <button onClick={() => setIsOpen(true)} className="mt-4 px-4 py-2 bg-[#F4F4F5] text-[#18181B] rounded-md">
+        Edit Page
+      </button>
       <EditProfile isOpen={isOpen} onClose={() => setIsOpen(false)} />
-        <CreatorPage/>
+      <CreatorPage />
     </div>
-    
-  )
+  );
 }
