6b878462b39b3a4a12810f80a1eba8d0bc090adc
Duu-Avia <duuavia01@gmail.com>
2025-02-08 22:56:41 +0800

    update

---
+++ src/app/explore/_components/NoCreator.tsx
@@ -0,0 +1,7 @@
+export const NoCreator = () =>{
+    return(
+    <div className="flex flex-col items-center">
+   <img className="size-[64px]" src="ExploreUserLogo.png" alt="Default User"/>
+    <div className="pt-[20px]">No creators have signed up yet</div>    
+    </div>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,28 @@ import Image from "next/image";
 import { useState } from "react";
 import EditProfile from "./_components/EditProfile";
 import { CreatorPage } from "./_components/creator-page/CreatorPage";
+import { HomeNavbar } from "./_components/HomeNavbar";
+import { HomeHero } from "./_components/HomeHero";
+import { HomeFeature } from "./_components/HomeFeature";
+import { HomeFooter } from "./_components/HomeFooter";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <div>
-      hi2 hello
-      <button onClick={() => setIsOpen(true)} className="mt-4 px-4 py-2 bg-[#F4F4F5] text-[#18181B] rounded-md">
-        Edit Page
-      </button>
-      <EditProfile isOpen={isOpen} onClose={() => setIsOpen(false)} />
-      <CreatorPage />
-    </div>
+  return (<>
+      <HomeNavbar/>
+      <HomeHero/>
+      <HomeFeature/>
+      <HomeFooter/>
+  </>
+
+    // <div>
+    //   hi2 hello
+    //   <button onClick={() => setIsOpen(true)} className="mt-4 px-4 py-2 bg-[#F4F4F5] text-[#18181B] rounded-md">
+    //     Edit Page
+    //   </button>
+    //   <EditProfile isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    //   <CreatorPage />
+    // </div>
   );
 }
