55a07b4bbba25ef1959b6222987155ef01510a78
24LP5392 24LP5392 <24LP5392@G304-CC0117.local>
2025-02-18 18:08:06 +0800

    02/18

--- src/app/profile-setup/_components/profile_setup_form_step2.tsx
+++ src/app/profile-setup/_components/profile_setup_form_step2.tsx
@@ -104,6 +104,8 @@ export default function ProfileSetup2() {
       setUserId(localId);
     }
   }, []);
+
+  console.log(paymentSchema)
   const sendDatas = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/profile`, {
       method: "POST",
