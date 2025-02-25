1da43bf749d8e07da3045f2672f951a8cef6bcfb
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2025-02-25 16:41:54 +0800

    finished last things3

--- src/app/explore/_components/NoCreator.tsx
+++ src/app/explore/_components/NoCreator.tsx
@@ -1,7 +1,8 @@
-export const NoCreator = () =>{
-    return(
+export const NoCreator = () => {
+  return (
     <div className="flex flex-col items-center">
-   <img className="size-[64px]" src="ExploreUserLogo.png" alt="Default User"/>
-    <div className="pt-[20px]">No creators have signed up yet</div>    
-    </div>)
-}
+      <img className="size-[64px]" src="ExploreUserLogo.png" alt="Default User" />
+      <div className="pt-[20px]">No creators have signed up yet please wait...</div>
+    </div>
+  );
+};
